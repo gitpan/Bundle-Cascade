64c93a786abe4b8f6c63d3ac28fd2cc76014bde7
Mark Stosberg <markstos@cpan.org>
2001-09-29 18:30:15 +0000

    Import of MARKSTOS/Bundle-Cascade-1.00 from CPAN.
    gitpan-cpan-distribution: Bundle-Cascade
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         MARKSTOS/Bundle-Cascade-1.00.tar.gz
    gitpan-cpan-author:       MARKSTOS
    gitpan-cpan-maturity:     released

---
+++ Cascade.pm
@@ -0,0 +1,52 @@
+package Bundle::Cascade;
+
+$Bundle::Cascade::VERSION = '1.00';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Cascade - A bundle to install all modules used for Cascade
+
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install "Bundle::Cascade"'>
+
+=head1 CONTENTS
+
+CGI 2.75
+
+HTML::Template 2.2
+
+CGI::Application 2.0
+
+DBIx::Abstract 1.001
+
+Digest::SHA1 1.03
+
+DBI 1.18
+
+HTML::FillInForm .18
+
+Data::FormValidator 1.5
+
+=head1 DESCRIPTION
+
+This bundle includes all the modules used by Cascade, created by Mark Stosberg.
+More information is available at http://summersault.com/software/cascade/
+ 
+A I<Bundle> is a module that simply defines a collection of other
+modules.  It is used by the L<CPAN> module to automate the fetching,
+building and installing of modules from the CPAN ftp archive sites.
+
+=head1 AUTHOR
+
+Mark Stosberg
+
+=cut 
+
+
+
